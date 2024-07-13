bbb719ad9603281eb9883ba56b00d1bc301ea74c
Raheem <rchisman@microsoft.com>
2024-07-13 15:00:29 -0700

    Alerts and logic test

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var NameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var password2TextField: UITextField!
+    
+    let alert = UIAlertController(title: "Error!", message: "Username not found!", preferredStyle: UIAlertController.Style.alert)
+    let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { UIAlertAction in
+        // button clicked
+        print("button clicked")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signUpButton(_ sender: Any) {
+        if passwordTextField.text == "" {
+            print("No user found.")
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+        if passwordTextField.text != password2TextField.text
+        {
+            
+            print ("No user found.")
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+        else
+        {print("success")}
+    }
 }
 
